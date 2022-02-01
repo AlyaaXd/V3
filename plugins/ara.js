cbac7290d372db13bc39c01afb484618431a84bc
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-02-01 22:34:43 +0700

    Delete plugins directory

--- plugins/ara.js
+++
@@ -1,9 +0,0 @@
-let fs = require('fs')
-let handler = async (m) => {
-let stc = fs.readFileSync('./audio/araara.m4a')
-conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*Ara Ara*', 'status@broadcast')
-}
-handler.customPrefix = /ara ara|araara|ara2|araara|arara/i
-handler.command = new RegExp
-
-module.exports = handler
--- plugins/invite.js
+++
@@ -1,7 +0,0 @@
-let handler = m => m.reply(`Gunakan !join5 linkgrub untuk memasukan Bot`)
-
-handler.customPrefix = /^Undangan/i
-handler.command = new RegExp
-handler.premium = true
-
-module.exports = handler
