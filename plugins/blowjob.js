648c46c949ad36edc462a8ac2335876ddd6c430f
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-02-01 22:41:23 +0700

    Add files via upload

---
+++ plugins/blowjob.js
@@ -0,0 +1,20 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, usedPrefix, command }) => {
+	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
+   heum = await fetch(`https://api.lolhuman.xyz/api/random2/bj?apikey=rey2k21`)
+    json = await heum.buffer()
+   conn.sendButtonImg(m.chat, json, 'Ahh jadi sange kak', watermark, 'NEXT', `${usedPrefix + command}`, m)
+}
+handler.help = ['blowjob']
+handler.tags = ['hentai']
+
+handler.command = /^(blowjob)$/i
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = 30
+
+module.exports = handler
+
---
+++ plugins/darkjokes.js
@@ -0,0 +1,12 @@
+let fetch = require('node-fetch')
+let handler  = async (m, { conn, usedPrefix, command }) => {
+conn.sendButtonImg(m.chat, await ( await fetch(global.API('xteam', '/asupan/darkjoke', {}, 'APIKEY'))).buffer(), 'Drag joles', watermark, 'NEXT', `${usedPrefix + command}`, m)
+}
+handler.help = ['darkjokes', 'darkjokes']
+handler.tags = ['internet','image']
+handler.command = /^(darkjoke|darkjokes)$/i
+handler.register = true
+
+handler.limit = true
+
+module.exports = handler
