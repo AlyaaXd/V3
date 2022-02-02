05d21962ee5caa14cf920762579eff8e4d852be8
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-02-02 10:48:56 +0700

    Update toimg2.js

--- plugins/toimg2.js
+++ plugins/toimg2.js
@@ -24,7 +24,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   if (/webp/.test(mime)) {
     out = await webp2png(media)
   }
-  await conn.sendFile(m.chat, out, 'out.png', '©RadBotZ', anu, false, { thumbnail: fs.readFileSync('./src/RadBot.png')})
+  await conn.sendFile(m.chat, out, 'out.png', '©AlyaaXzy', anu, false, { thumbnail: fs.readFileSync('./src/RadBot.png')})
 }
 handler.help = ['toimg']
 handler.tags = ['sticker']
