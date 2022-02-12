14a29d05b7f5154276d3579bc4e60099b500a678
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-02-12 21:59:10 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -72,7 +72,7 @@ handler.all = async function (m, { isBlocked }) {
     if (new Date() * 1 - setting.status > 1000) {
         let _uptime = process.uptime() * 1000
         let uptime = clockString(_uptime)
-        await this.setStatus(`Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} |AlyaaXzyBotz by AlyaaXzy`).catch(_ => _)
+        await this.setStatus(`💠Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} |AlyaaXzy by R-Txzy Team`).catch(_ => _)
         setting.status = new Date() * 1
     }
 
