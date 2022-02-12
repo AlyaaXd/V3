fb24d6eb334e6716dcf5a3f0818b41df3503f9b5
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-02-13 01:05:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -84,7 +84,7 @@ global.image = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg' //change the 
 global.thumbfoto = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg'
 
 // Ubah saja
-global.image = 'https://telegra.ph/file/338349c86c4fcd540968b.png'//thumbnail
+global.image = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg'//thumbnail
 global.bank = 'https://telegra.ph/file/d5ddf4cc627bb0e6bc420.jpg'
 global.kandang = 'https://telegra.ph/file/67a6ee607d03a4e52757d.jpg'
 global.kolam = 'https://telegra.ph/file/5aa5dfa3394477e11fb18.jpg'
