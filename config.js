ce89ccb4261c16f8531f8984cd8367fc1c04e9e8
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-02-12 23:10:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -76,8 +76,8 @@ global.eror = '_*Error*_'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.watermark = '© R-Txzy' //change the watermark 
-global.image = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg' //change the image
-global.thumbfoto = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg'
+global.image = 'https://telegra.ph/file/ae60c29d9e1b4e159f90f.jpg' //change the image
+global.thumbfoto = 'https://telegra.ph/file/ae60c29d9e1b4e159f90f.jpg'
 
 // image
 global.bank = 'https://telegra.ph/file/d5ddf4cc627bb0e6bc420.jpg'
