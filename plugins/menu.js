243e6e7ba3daa5ec775d4b3d8606283fde7df601
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-02-12 21:45:57 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -25,7 +25,7 @@ const defaultMenu = {
 *└───────────────────•*
 %readmore`.trimStart(),
   header: '*┌─〔 %category 〕*',
-  body: '*│*⦁ %cmd %islimit %isPremium',
+  body: '*│▸* %cmd %islimit %isPremium',
   footer: '*└────*\n',
   after: `
 *%npmname@^%version*
