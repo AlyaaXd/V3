14261fc0a50c95686e58249bca5d338b66b0efa1
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-02-13 12:26:30 +0700

    Delete owneryve.js

--- plugins/owneryve.js
+++
@@ -1,8 +0,0 @@
-let handler = function (m) {
-	this.sendContact(m.chat, '994409869765', 'Owner RadBotZ :)', m)
-}
-
-handler.customPrefix = ['🍭Owner Mike'] 
-handler.command = new RegExp
-
-module.exports = handler
