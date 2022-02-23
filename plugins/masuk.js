d87732b7c2e796eeb9d7ab8527f2c59b311ec5a7
AlyaaXzy <96462315+AlyaaXd@users.noreply.github.com>
2022-02-23 13:26:49 +0700

    Update masuk.js

--- plugins/masuk.js
+++ plugins/masuk.js
@@ -1,7 +1,7 @@
 function handler(m) {
     m.reply('Konfirmasi ke owner untuk informasi lebih lanjut.')
     this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
-    this.sendContact(m.chat, '62858929626673@s.whatsapp.net', 'Raditya', m)
+    this.sendContact(m.chat, '6289505165400@s.whatsapp.net', 'AlyaaXzy', m)
   }
   handler.command = /^(masuk)$/i
   
