248b4ab80317a0a7ca9ca4af2843ecb4601f062b
Arullzzz <106241765+Arullzzz@users.noreply.github.com>
2022-05-30 23:03:31 +0800

    Update and rename plugins/masuk.js to plugin/masuk.js

--- plugin/masuk.js
+++ plugin/masuk.js
@@ -1,7 +1,7 @@
 function handler(m) {
     m.reply('Konfirmasi ke owner untuk informasi lebih lanjut.')
     this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
-    this.sendContact(m.chat, '6289505165400@s.whatsapp.net', 'AlyaaXzy', m)
+    this.sendContact(m.chat, '6285822347348@s.whatsapp.net', 'ArullWara', m)
   }
   handler.command = /^(masuk)$/i
   
