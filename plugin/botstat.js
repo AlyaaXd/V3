8fe0f1a3dad13ffe797effa097a8f2d69dcb89ef
Arullzzz <106241765+Arullzzz@users.noreply.github.com>
2022-05-30 22:55:08 +0800

    Update and rename plugins/botstat.js to plugin/botstat.js

--- plugin/botstat.js
+++ plugin/botstat.js
@@ -30,7 +30,7 @@ let handler = async (m, { conn }) => {
 ├ ${jadibot ? '✅' : '❌'} *Jadi Bot*
 ├ ${nsfw ? '✅' : '❌'} *Mode Nsfw*
 ╰────────────···`.trim()
-     await conn.send2ButtonLoc(m.chat, await(await fetch(image)).buffer(), str, '©R-Txzy', 'Owner', '.owner', 'Menu', '.menu', m)
+     await conn.send2ButtonLoc(m.chat, await(await fetch(image)).buffer(), str, '©ArullWara', 'Owner', '.owner', 'Menu', '.menu', m)
 }
 handler.help = ['botstatus']
 handler.tags = ['info']
